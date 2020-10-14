2efa946abc42ae421f8f1d6f32d60c9b305a44a3
TeddyBonkerz <radzlaif@gmail.com>
2020-10-14 15:21:32 -0400

    Deleted BusinessSetupModel and updated MainActivity for use as the Business Setup page

--- app/src/main/java/com/example/storefrontapp/MainActivity.java
+++ app/src/main/java/com/example/storefrontapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.storefrontapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,10 +12,23 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText mFirstName, mLastName, mStreet, mCity, mState, mPostalCode, mCountry;
+    Button mNextBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // instantiate the member variables
+        mFirstName = findViewById(R.id.firstName);
+        mLastName = findViewById(R.id.lastName);
+        mStreet = findViewById(R.id.street);
+        mCity = findViewById(R.id.city);
+        mState = findViewById(R.id.state);
+        mPostalCode = findViewById(R.id.postalCode);
+        mCountry = findViewById(R.id.country );
+        mNextBtn = findViewById(R.id.nextButton );
     }
 
     //Logout user
