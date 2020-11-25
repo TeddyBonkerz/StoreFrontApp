5fc88e095172981f83e979cc66d584ffc03095fa
TeddyBonkerz <radzlaif@gmail.com>
2020-11-24 20:43:50 -0500

    Finally implemented appbar. Adding Drawer layout next

--- app/src/main/java/com/example/storefrontapp/Home.java
+++ app/src/main/java/com/example/storefrontapp/Home.java
@@ -1,8 +1,10 @@
 package com.example.storefrontapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar, menu);
+        return true;
     }
 }
