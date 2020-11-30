6e92b94f911735c2ee76cf27888b6e6dfb3f6886
Efaz Khan <efazahmed@hotmail.com>
2020-11-30 17:49:23 -0500

    inventory initial setup done

--- app/src/main/java/com/example/storefrontapp/navdrawer/add_product_fragment.java
+++ app/src/main/java/com/example/storefrontapp/navdrawer/add_product_fragment.java
@@ -1,21 +1,142 @@
 package com.example.storefrontapp.navdrawer;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.storefrontapp.R;
+import com.example.storefrontapp.login;
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-public class add_product_fragment extends Fragment {
+import java.io.File;
+import java.util.UUID;
+
+import javax.xml.transform.Result;
+
+public class add_product_fragment extends Fragment implements AdapterView.OnItemSelectedListener{
+
+
+    String productType;
+
+    private ImageView productImg;
+    public Uri imageUri;
+    public Spinner product_type_spinner;
+
+    private FirebaseStorage mFirebaseStorage;
+    private StorageReference mStorageReference;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_product, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_product, container, false);
+
+        mFirebaseStorage = FirebaseStorage.getInstance();
+        mStorageReference = mFirebaseStorage.getReference();
+
+        productImg = (ImageView) view.findViewById(R.id.productImg);
+        productImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                choosePicture();
+            }
+        });
+
+
+        product_type_spinner = view.findViewById(R.id.product_type_spinner);
+        ArrayAdapter<CharSequence> adapter1 =  ArrayAdapter.createFromResource(getContext(), R.array.product_type_spinner, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        product_type_spinner.setAdapter(adapter1);
+        product_type_spinner.setOnItemSelectedListener(this);
+
+
+        return view;
+    }
+
+
+
+
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && data != null && data.getData() != null){
+            imageUri = data.getData();
+            productImg.setImageURI(imageUri);
+
+            uploadImg();
+        }
+    }
+
+    private void uploadImg() {
+        final ProgressDialog pd = new ProgressDialog(getActivity());
+        pd.setTitle("Uploading Image...");
+        pd.show();
+
+        final String randKey = UUID.randomUUID().toString();
+
+        StorageReference riversRef = mStorageReference.child("productImages/" + randKey);
+
+        riversRef.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        pd.dismiss();
+                        Toast.makeText(getActivity(), "Image Uploaded", Toast.LENGTH_LONG).show();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        pd.dismiss();
+
+                        Toast.makeText(getActivity(), "Error! " + exception.getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                        double progressPercent = (100.00 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                        pd.setMessage("Progress: " + (int) progressPercent + "%");
+                    }
+                });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        productType = adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
