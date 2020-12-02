40c342a00cbc44321efb990129b80984d424b03e
TeddyBonkerz <radzlaif@gmail.com>
2020-12-02 12:22:22 -0500

    Added inventory page and add product page.

--- app/src/main/java/com/example/storefrontapp/home.java
+++ app/src/main/java/com/example/storefrontapp/home.java
@@ -6,16 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.storefrontapp.navdrawer.account_fragment;
 import com.example.storefrontapp.navdrawer.add_product_fragment;
 import com.example.storefrontapp.navdrawer.home_fragment;
+import com.example.storefrontapp.navdrawer.inventory_fragment;
 import com.example.storefrontapp.navdrawer.payment_fragment;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -32,6 +36,9 @@ public class home extends AppCompatActivity implements NavigationView.OnNavigati
     private DrawerLayout drawer;
     private Toolbar toolbar;
     private NavigationView navigationView;
+    private ActionBarDrawerToggle toggle;
+    private boolean navBarRegistered = false;
+
     FirebaseAuth mAuth;
     FirebaseUser currentUser;
 
@@ -44,29 +51,30 @@ public class home extends AppCompatActivity implements NavigationView.OnNavigati
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
-        //userText = findViewById(R.id.welcomeUser);
-        //userText.setText(currentUser.getDisplayName());
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        if(savedInstanceState == null) {
+        if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new home_fragment()).commit();
             navigationView.setCheckedItem(R.id.home);
         }
-
 
 
     }
@@ -75,23 +83,26 @@ public class home extends AppCompatActivity implements NavigationView.OnNavigati
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.home:
+                getSupportActionBar().setTitle("Home");
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new home_fragment()).commit();
                 break;
             case R.id.account:
+                getSupportActionBar().setTitle("Account");
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new account_fragment()).commit();
                 break;
             case R.id.inventory:
+                getSupportActionBar().setTitle("Inventory");
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new add_product_fragment()).commit();
+                        new inventory_fragment()).commit();
                 break;
             case R.id.payment_details:
+                getSupportActionBar().setTitle("Payment Methods");
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new payment_fragment()).commit();
                 break;
             case R.id.logout:
-                FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(getApplicationContext(), login.class));
                 finish();
                 break;
@@ -114,5 +125,42 @@ public class home extends AppCompatActivity implements NavigationView.OnNavigati
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.appbar, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_product:
+                displayHomeUpOrHamburger();
+                getSupportActionBar().setTitle("Add Product");
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new add_product_fragment()).commit();
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void displayHomeUpOrHamburger() {
+        boolean upButton = getSupportFragmentManager().getBackStackEntryCount() > 0;
+
+        if (upButton) {
+            drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+            toggle.setDrawerIndicatorEnabled(false);
+            if (!navBarRegistered) {
+                toggle.setToolbarNavigationClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        getSupportFragmentManager().popBackStackImmediate();
+                    }
+                });
+                navBarRegistered = true;
+            }
+        } else {
+            drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+            toggle.setDrawerIndicatorEnabled(true);
+            toggle.setToolbarNavigationClickListener(null);
+            navBarRegistered = false;
+        }
     }
 }
--- app/src/main/java/com/example/storefrontapp/navdrawer/add_product_fragment.java
+++ app/src/main/java/com/example/storefrontapp/navdrawer/add_product_fragment.java
@@ -18,6 +18,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.storefrontapp.InventoryModel;
