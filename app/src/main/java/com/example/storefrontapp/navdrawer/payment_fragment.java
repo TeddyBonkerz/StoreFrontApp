aa4f1f9e525009edefdf03d2e3835aa083c5742f
Efaz Khan <efazahmed@hotmail.com>
2020-12-03 02:02:17 -0500

    updated Payment Page

--- app/src/main/java/com/example/storefrontapp/navdrawer/payment_fragment.java
+++ app/src/main/java/com/example/storefrontapp/navdrawer/payment_fragment.java
@@ -1,14 +1,20 @@
 package com.example.storefrontapp.navdrawer;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.braintreepayments.cardform.view.CardForm;
 import com.example.storefrontapp.R;
 
 public class payment_fragment extends Fragment {
@@ -16,6 +22,55 @@ public class payment_fragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_payment, container, false);
+        View view = inflater.inflate(R.layout.fragment_payment, container, false);
+
+        final CardForm cardForm = view.findViewById(R.id.card_form);
+        Button linkCard = view.findViewById(R.id.linkCard);
+
+        cardForm.cardRequired(true)
+                .expirationRequired(true)
+                .cvvRequired(true)
+                .postalCodeRequired(true)
+                .mobileNumberRequired(true)
+                .mobileNumberExplanation("Text SMS is required on this number")
+                .setup(getActivity());
+
+        cardForm.getCvvEditText().setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+
+        linkCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (cardForm.isValid()) {
+
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getContext());
+                    alertBuilder.setTitle("Please Confirm Your Info");
+                    alertBuilder.setMessage("Card number: " + cardForm.getCardNumber() + "\n" +
+                            "Card expiry date: " + cardForm.getExpirationDateEditText().getText().toString() + "\n" +
+                            "Card CVV: " + cardForm.getCvv() + "\n" +
+                            "Postal code: " + cardForm.getPostalCode() + "\n" +
+                            "Phone number: " + cardForm.getMobileNumber());
+                    alertBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            Toast.makeText(getActivity(), "Card Linked Successfully", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    AlertDialog alertDialog = alertBuilder.create();
+                    alertDialog.show();
+
+                }else {
+                    Toast.makeText(getActivity(), "Please complete the form", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        return view;
     }
 }
